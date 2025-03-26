51f68bec512c756d2fec842948de97b601388201
JOAO VICTOR ALVES DOS SANTOS <hyperlobe2@gmail.com>
2025-03-26 10:36:32 -0300

    feat: initial project structure

---
+++ index.js
@@ -0,0 +1,6 @@
+const root = document.getElementById("root");
+
+const calendarContainer = document.createElement("div");
+calendarContainer.className = "calendar-container";
+
+root.appendChild(calendarContainer);
